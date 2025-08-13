7b4dc88c664b996f4173c545b087473c1583519e
Jose <68226374+know-it-all-n1@users.noreply.github.com>
2025-08-13 22:14:50 +0200

    Including the Peterson Algorithm for testing

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.example.CriticalSectionProblem.DekkerAlgorithm;
+import org.example.CriticalSectionProblem.PetersonAlgorithm;
 
 public class App {
     public static void main(String[] args) {
@@ -8,5 +9,10 @@ public class App {
         DekkerAlgorithm dkAlgo = new DekkerAlgorithm();
         System.out.println("Testing Dekker Algorithm:\n");
         dkAlgo.run();
+
+        // Peterson Algorithm
+        PetersonAlgorithm petersonAlgo = new PetersonAlgorithm();
+        System.out.println("Testing Peterson Algorithm:\n");
+        petersonAlgo.run();
     }
 }
