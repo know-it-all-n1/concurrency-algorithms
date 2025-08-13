843141185459430b1216c3e03a54b9e1cb30fd22
Jose <68226374+know-it-all-n1@users.noreply.github.com>
2025-08-13 22:16:14 +0200

    Added line breaks to improve readability

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,12 +7,12 @@ public class App {
     public static void main(String[] args) {
         // Dekker Algorithm
         DekkerAlgorithm dkAlgo = new DekkerAlgorithm();
-        System.out.println("Testing Dekker Algorithm:\n");
+        System.out.println("\nTesting Dekker Algorithm:\n");
         dkAlgo.run();
 
         // Peterson Algorithm
         PetersonAlgorithm petersonAlgo = new PetersonAlgorithm();
-        System.out.println("Testing Peterson Algorithm:\n");
+        System.out.println("\nTesting Peterson Algorithm:\n");
         petersonAlgo.run();
     }
 }
