7ba031b138885067950a6d02339bfbc2b1cacd5f
Jose <68226374+know-it-all-n1@users.noreply.github.com>
2025-08-13 19:37:32 +0200

    Added Dekker Algorithm implementation

---
+++ src/main/java/org/example/CriticalSection.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class CriticalSection {
+    private boolean occupied;
+
+    public CriticalSection() {
+        occupied = false;
+    }
+
+    public synchronized void occupy(String process) throws MutualExclusionInfringement {
+        if (occupied) throw new MutualExclusionInfringement();
+        occupied = true;
+        System.out.println(process + " is occupying the critical section");
+    }
+
+    public synchronized void abandon() {
+        occupied = false;
+    }
+}
---
+++ src/main/java/org/example/DekkerAlgorithm/DekkerAlgorithm.java
@@ -0,0 +1,146 @@
+package org.example.DekkerAlgorithm;
+
+import org.example.CriticalSection;
+
+/**
+ * Implementation of the Dekker algorithm with demonstration purposes<br>
+ *
+ * <b>Note:</b> await statements in the original design of the algorithm have been
+ * implemented as while loops.
+ */
+public class DekkerAlgorithm {
+    private boolean wantp;
+    private boolean wantq;
+    private Turn turn;
+    private CriticalSection cs;
+
+    public DekkerAlgorithm() {
+        this.turn = new Turn(1);
+        wantp = false;
+        wantq = false;
+    }
+
+    public void run() {
+        Thread p = new Thread(new ProcessP());
+        Thread q = new Thread(new ProcessQ());
+
+        p.start();
+        q.start();
+    }
+
+    private class ProcessP implements Runnable {
+        @Override
+        public void run() {
+            // Instead of an infinite loop we will use a finite loop
+            // for demonstration purposes
+            // loop "forever"
+            for (int i = 0; i < 5; i++) {
+                try {
+                    // non-critical section
+                    System.out.println("Process P in the NCS");
+                    Thread.sleep(1000);
+
+                    // wantp <- true
+                    wantp = true;
+
+                    // while wantq
+                    while (wantq) {
+
+                        // if turn = 2
+                        if (turn.getTurn() == 2) {
+
+                            // wantp <- false
+                            wantp = false;
+
+                            // await turn = 1
+                            while (turn.getTurn() != 1) {
+                                Thread.sleep(500);
+                            }
+
+                            // wantp <- true
+                            wantp = true;
+                        }
+
+                        // critical section
+                        cs.occupy("Process P");
+
+                        // turn <- 2
+                        turn.setTurn(2);
+
+                        // wantp <- false
+                        wantp = false;
+                    }
+                } catch (InterruptedException e) {
+                    // IGNORED
+                }
+
+            }
+        }
+    }
+
+    private class ProcessQ implements Runnable {
+        @Override
+        public void run() {
+            // Instead of an infinite loop we will use a finite loop
+            // for demonstration purposes
+            // loop "forever"
+            for (int i = 0; i < 5; i++) {
+                try {
+                    // non-critical section
+                    System.out.println("Process Q in the NCS");
+                    Thread.sleep(1000);
+
+                    // wantq <- true
+                    wantq = true;
+
+                    // while wantq
+                    while (wantq) {
+
+                        // if turn = 1
+                        if (turn.getTurn() == 1) {
+
+                            // wantq <- false
+                            wantq = false;
+
+                            // await turn = 1
+                            while (turn.getTurn() != 2) {
+                                Thread.sleep(500);
+                            }
+
+                            // wantq <- true
+                            wantq = true;
+                        }
+
+                        // critical section
+                        cs.occupy("Process Q");
+
+                        // turn <- 1
+                        turn.setTurn(1);
+
+                        // wantq <- false
+                        wantq = false;
+                    }
+                } catch (InterruptedException e) {
+                    // IGNORED
+                }
+
+            }
+        }
+    }
+
+    private class Turn {
+        private int turn;
+
+        public Turn(int turn) {
+            this.turn = turn;
+        }
+
+        public synchronized int getTurn() {
+            return turn;
+        }
+
+        public synchronized void setTurn(int turn) {
+            this.turn = turn;
+        }
+    }
+}
