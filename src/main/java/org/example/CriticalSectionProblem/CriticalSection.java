14fe42eb7d02b8c843545080dfd48412a768a8b9
Jose <68226374+know-it-all-n1@users.noreply.github.com>
2025-08-20 21:09:07 +0200

    Modified to store the process which is in it, so it can be displayed when exiting.

--- src/main/java/org/example/CriticalSectionProblem/CriticalSection.java
+++ src/main/java/org/example/CriticalSectionProblem/CriticalSection.java
@@ -2,6 +2,7 @@ package org.example.CriticalSectionProblem;
 
 public class CriticalSection {
     private boolean occupied;
+    private String process;
 
     public CriticalSection() {
         occupied = false;
@@ -10,10 +11,12 @@ public class CriticalSection {
     public void occupy(String process) throws MutualExclusionInfringement {
         if (occupied) throw new MutualExclusionInfringement();
         occupied = true;
+        this.process = process;
         System.out.println(process + " is occupying the critical section");
     }
 
     public void abandon() {
+        System.out.println(process + " is exiting the critical section");
         occupied = false;
     }
 }
